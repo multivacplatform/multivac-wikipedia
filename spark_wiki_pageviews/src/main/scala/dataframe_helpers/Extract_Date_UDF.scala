1ceb96b152e18dbbe8ef28bfec7166c4c5b59b6a
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-04 01:29:05 +0100

    add UDF to extract filename

--- spark_wiki_pageviews/src/main/scala/dataframe_helpers/Extract_Date_UDF.scala
+++ spark_wiki_pageviews/src/main/scala/dataframe_helpers/Extract_Date_UDF.scala
@@ -1,5 +1,21 @@
 package dataframe_helpers
+import org.apache.spark.sql.functions.udf
 
 object Extract_Date_UDF {
-
+  def get_year_file_name = udf { fullPath: String =>
+    val str = fullPath.split("/").last
+    str.split("-")(1).toArray.slice(0, 4).mkString
+  }
+  def get_month_file_name = udf { fullPath: String =>
+    val str = fullPath.split("/").last
+    str.split("-")(1).toArray.slice(4, 6).mkString
+  }
+  def get_day_file_name = udf { fullPath: String =>
+    val str = fullPath.split("/").last
+    str.split("-")(1).toArray.slice(6, 8).mkString
+  }
+  def get_hour_file_name = udf { fullPath: String =>
+    val str = fullPath.split("/").last
+    str.split("-")(2).toArray.slice(0, 2).mkString
+  }
 }
