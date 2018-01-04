e822b2ba0de07eefa54b060a498e2aa98b2a38be
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-05 00:07:37 +0100

    add write partitioned parquet and read from it

--- spark_wiki_pageviews/src/main/scala/dataframe_helpers/Parquet_helper.scala
+++ spark_wiki_pageviews/src/main/scala/dataframe_helpers/Parquet_helper.scala
@@ -1,14 +1,28 @@
 package dataframe_helpers
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object Parquet_helper {
   def saveDataFrameAsParquet(
                               wikiDF: DataFrame,
-                              demoFilePath: String
+                              demoFilePath: String,
+                              spark: SparkSession
                             ): Unit ={
+    import spark.implicits._
 
-    wikiDF.write.mode("overwrite").parquet(demoFilePath)
+    wikiDF
+      .repartition($"year", $"month", $"day")
+      .write
+      .mode(SaveMode.Overwrite)
+      .option("compression","snappy")
+      .partitionBy("year", "month", "day")
+      .parquet(demoFilePath)
+  }
 
+  def readParquetAsDataFrame(
+                              demoFilePath: String,
+                              spark: SparkSession
+                            ): DataFrame ={
+    spark.read.parquet(demoFilePath)
   }
 
 }
