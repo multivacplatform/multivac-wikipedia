bebb0ca40e84f0f22b7130c724397dea01748beb
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-04 01:29:54 +0100

    test creating df from file path in config

--- spark_wiki_pageviews/src/main/scala/Main.scala
+++ spark_wiki_pageviews/src/main/scala/Main.scala
@@ -1,8 +1,19 @@
+import com.typesafe.config.ConfigFactory
+import dataframe_helpers.Cleaning_Data
 import spark_helpers.SessionBuilder
 
 object Main {
   def main(args: Array[String]) {
     val env = args(0)
     val spark = SessionBuilder.buildSession(env)
+
+
+
+    var demoFilePath = ConfigFactory.load().getString("spark.local.pageviewsPath.value")
+
+    Cleaning_Data.create_dataframe(
+      demoFilePath,
+      spark
+    )
   }
 }
