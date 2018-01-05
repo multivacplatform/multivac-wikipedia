8e65ee84989c81ff252e102a62d9be1ded2d792d
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-05 15:13:57 +0100

    fix naming convention

--- spark_wiki_pageviews/src/main/scala/Main.scala
+++ spark_wiki_pageviews/src/main/scala/Main.scala
@@ -1,16 +1,16 @@
 import com.typesafe.config.ConfigFactory
-import dataframe_helpers.{Cleaning_Data, Parquet_helper}
+import dataframe_helpers.{InputHelper, ParquetHelper}
 
-import spark_helpers.SessionBuilder
+import spark_helpers.SparkSessionHelper
 import org.apache.spark.sql.functions._
 
 object Main {
   def main(args: Array[String]) {
     val env = args(0)
-    val spark = SessionBuilder.buildSession(env)
+    val spark = SparkSessionHelper.buildSession(env)
     val demoFilePath = ConfigFactory.load().getString("spark.local.pageviewsPath.value")
 
-    val wikiPageViewsDF = Cleaning_Data.create_dataframe(
+    val wikiPageViewsDF = InputHelper.create_dataframe(
       demoFilePath,
       spark
     )
@@ -41,8 +41,8 @@ object Main {
 
     // save as a parquet
     val parguetPath = ConfigFactory.load().getString("spark.local.parquetOutPutPath.value")
-    Parquet_helper.saveDataFrameAsParquet(wikiPageViewsDF, parguetPath, spark)
-    val pageViewsParquetDF = Parquet_helper.readParquetAsDataFrame(parguetPath, spark)
+    ParquetHelper.saveDataFrameAsParquet(wikiPageViewsDF, parguetPath, spark)
+    val pageViewsParquetDF = ParquetHelper.readParquetAsDataFrame(parguetPath, spark)
 
     println("========================== \n")
     println("========================== \n")
