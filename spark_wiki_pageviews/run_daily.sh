c54ac255b7ea69fe71858dd8eca7cbcab2ccc3fb
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-08 12:40:48 +0100

    fix bash compatibility darwin and linux-gnu

--- spark_wiki_pageviews/run_daily.sh
+++ spark_wiki_pageviews/run_daily.sh
@@ -4,14 +4,29 @@
 ##############################################################################
 #!/usr/bin/env bash
 
-Y=$(date +'%Y')
-M=$(date +'%m')
-D=$(date -v-1d +'%d')
+
 
 now="$(date +'%d-%m-%Y')"
 echo "today is $now"
 
-yesterday="$(date -v-1d +%F)"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+echo "OSType is $OSTYPE"
+
+yesterday=$(date -d '1 days ago' +'%d-%m-%Y')
+Y=$(date +'%Y')
+M=$(date +'%m')
+D=$(date -d '1 days ago' +%d)
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+echo "OSType is $OSTYPE"
+
+yesterday="$(date -v1d +%F)"
+Y=$(date +'%Y')
+M=$(date +'%m')
+D=$(date -v-1d +'%d')
+fi
+
 echo "yesterdy's date is $yesterday"
 
 echo "YEAR: $Y MONTH: $M DAY: $D"
@@ -21,6 +36,7 @@ rm data/pageviews/*.gz
 echo "old downloads has been cleaned"
 
 echo "start downloading yesterday's pageviews"
+
 
 #sample hourly file: https://dumps.wikimedia.org/other/pageviews/2018/2018-01/pageviews-20180107-000000.gz
 for i in 0{0..9}0000 {10..23}0000 ; do
