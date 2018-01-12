880d862ebcd709237329128c5a9eab601c8238d7
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-12 17:33:56 +0100

    let's clean existing pageviews in download script
    in case HDFS put failed

--- build_pageviews/prepare_hdfs.sh
+++ build_pageviews/prepare_hdfs.sh
@@ -2,6 +2,5 @@
 
 hadoop fs -rm -r -skipTrash /tmp/wiki
 hadoop fs -mkdir /tmp/wiki/
-hadoop fs -put data/pageviews/* /tmp/wiki/
-rm data/pageviews/*.gz
+hadoop fs -put data/pageviews/ /tmp/wiki/
 hadoop fs -put -f target/scala-2.11/*.jar /tmp/wiki/
