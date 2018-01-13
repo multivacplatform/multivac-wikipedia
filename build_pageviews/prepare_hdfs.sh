9843c0597857af4bbcdbf35bf4df40e4ceb3fe80
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-13 12:13:59 +0100

    update with displaying info

--- build_pageviews/prepare_hdfs.sh
+++ build_pageviews/prepare_hdfs.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+echo "cleaning /tmp/wiki on HDFS"
 hadoop fs -rm -r -skipTrash /tmp/wiki
+echo "creating /tmp/wiki on HDFS"
 hadoop fs -mkdir /tmp/wiki/
+echo "copying data/pageviews to /tmp/wiki/pageviews on HDFS"
 hadoop fs -put data/pageviews/ /tmp/wiki/
+echo "copying target/scala-2.11/*.jar to /tmp/wiki on HDFS"
 hadoop fs -put -f target/scala-2.11/*.jar /tmp/wiki/
+echo "preparation has been done!"
