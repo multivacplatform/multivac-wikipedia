45eadfaa561be07e6ab665fbf4040f8dec4213f6
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-13 14:04:26 +0100

    only copy gz files

--- build_pageviews/prepare_hdfs.sh
+++ build_pageviews/prepare_hdfs.sh
@@ -5,7 +5,7 @@ hadoop fs -rm -r -skipTrash /tmp/wiki
 echo "creating /tmp/wiki on HDFS"
 hadoop fs -mkdir /tmp/wiki/
 echo "copying data/pageviews to /tmp/wiki/pageviews on HDFS"
-hadoop fs -put data/pageviews/ /tmp/wiki/
+hadoop fs -put data/pageviews/*.gz /tmp/wiki/
 echo "copying target/scala-2.11/*.jar to /tmp/wiki on HDFS"
 hadoop fs -put -f target/scala-2.11/*.jar /tmp/wiki/
 echo "preparation has been done!"
