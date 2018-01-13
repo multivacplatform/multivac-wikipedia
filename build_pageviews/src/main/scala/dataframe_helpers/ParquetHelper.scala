7b3c6fe8704b954ddd422b70152626a4d2c1b5fc
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-13 14:03:07 +0100

    repartition by "day" before saving

--- build_pageviews/src/main/scala/dataframe_helpers/ParquetHelper.scala
+++ build_pageviews/src/main/scala/dataframe_helpers/ParquetHelper.scala
@@ -2,6 +2,7 @@ package dataframe_helpers
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 object ParquetHelper {
+
   def saveDataFrameAsParquet(
                               year: Int,
                               month: Int,
@@ -14,16 +15,19 @@ object ParquetHelper {
 
     val partitionPath = demoFilePath + "/year=" + year + "/month=" + month + "/day=" + day
     println("daily partiton path ", partitionPath)
-    wikiDF
+    val tempDF = wikiDF
       .filter($"year" === year)
       .filter($"month" === month)
       .filter($"day" === day)
+
+    tempDF.repartition($"day")
+
+    tempDF
       .write
-//      .partitionBy("day") // not needed since we filter and overwrite into one partition
+      //.partitionBy("day") // not needed since we filter and overwrite into one partition
       .mode(SaveMode.Overwrite)
       .option("compression","snappy")
       .parquet(s"$partitionPath")
-
   }
 
   def readParquetAsDataFrame(
@@ -32,5 +36,4 @@ object ParquetHelper {
                             ): DataFrame ={
     spark.read.parquet(demoFilePath)
   }
-
 }
