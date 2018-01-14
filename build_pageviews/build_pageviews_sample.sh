2f2f2ec8e92925afa76092226c03c8e0dafa4b63
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-14 12:59:47 +0100

    sample script to run on daily basis

--- build_pageviews/build_pageviews_sample.sh
+++ build_pageviews/build_pageviews_sample.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+echo "start downloading yesterday's pageviews"
+./download_daily.sh
+echo "start pulling latest code, cleaning and creating fat JAR"
+./build_lib.sh
+#change the following base on your environment: local / cluster
+echo "start preparing everything for HDFS"
+./prepare_hdfs.sh
+echo "start Spark job on YARN cluster"
+./start_spark.sh -m yarn -i hdfs:///tmp/wiki/*.gz -o hdfs:///path-to-output -j hdfs:///tmp/wiki/build_pageviews-assembly-0.1.jar
